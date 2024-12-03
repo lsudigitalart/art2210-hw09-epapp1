65eb23c9f43284b8f1d106fe7d1d78c15e439e12
“Ellie <epapp1@lsu.edu>
2024-12-03 15:27:09 -0600

    final

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var img1;
 var img2;
 var img3;
 var img4;
+var img5;
 
 
 function preload() {
@@ -9,25 +10,41 @@ function preload() {
     img2 = loadImage("Cat2.png");
     img3 = loadImage("Cat3.png");
     img4 = loadImage("Cat4.png");
+    img5 = loadImage("grass.jpg");
 }
 
 function setup() {
-    createCanvas(400, 600);
+    createCanvas(900, 600);
 
-    let randSpeed1 = random(0.5, 5);
-    let randSpeed2 = random(0.5, 5);
-    let randSpeed3 = random(0.5, 5);
-    let randSpeed4 = random(0.5, 5);
+    let randSpeed1 = random(5, 30);
+    let randSpeed2 = random(5, 30);
+    let randSpeed3 = random(5, 30);
+    let randSpeed4 = random(5, 30);
 
-    cat1 = new Cat(img1, 20, 50, randSpeed1);
-    cat2 = new Cat(img2, 20, 150, randSpeed2);
-    cat3 = new Cat(img3, 20, 250, randSpeed3);
-    cat4 = new Cat(img4, 20, 350, randSpeed4);
+    cat1 = new Cat(img1, 900, -100, randSpeed1);
+    cat2 = new Cat(img2, 900, 50, randSpeed2);
+    cat3 = new Cat(img3, 900, 150, randSpeed3);
+    cat4 = new Cat(img4, 900, 100, randSpeed4);
 
 }
 
 function draw() {
-    background(140, 175, 186);
+    background(img5);
+
+    cat1.move();
+    cat1.display();
+
+    cat2.move();
+    cat2.display();
+    
+    cat3.move();
+    cat3.display();
+
+    cat4.move();
+    cat4.display();
+
+
+
 
 }
 
@@ -38,7 +55,7 @@ function Cat(img, tempX, tempY, tempSpeed) {
     this.speed = tempSpeed;
 
     this.move = function(){
-        this.xpos += random(-this.speed, this.speed);
+        this.xpos += random(-this.speed);
     }
 
     this.display = function(){
